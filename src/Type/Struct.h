fbf3c5a992798e5c926dd8c7e44a0eaa95b5dd0b
SilverJun <jangej1031@naver.com>
2017-05-08 10:42:12 +0900

    Fix Struct and doc

--- src/Type/Struct.h
+++ src/Type/Struct.h
@@ -9,11 +9,13 @@
 
 namespace swing
 {
-
 	class Struct
 	{
 	protected:
+		bool _isSetBody = false;
+		llvm::StructType* _type;
 		std::vector<StoredProperty> _value;
+		std::vector<llvm::Type*> _typeLayout;
 		std::unordered_map<std::string, std::unique_ptr<llvm::Function> > _method;
 
 		/// Protocol conform _protocols
@@ -21,13 +23,32 @@ namespace swing
 	public:
 		Struct(std::string name, std::vector<llvm::Type*> val, std::unordered_map<std::string, std::unique_ptr<llvm::Function> > method) :
 			_value(),
+			_typeLayout(val),
 			_method(method)
 		{
-			llvm::StructType::create()
+			_type = llvm::StructType::create(g_Context, name);
+		}
+		virtual ~Struct() {}
 
+		void appendType(llvm::Type* type) { _typeLayout.push_back(type); }
+
+		/**
+		 * \brief Set struct body. this method is call once at last Generate time.
+		 */
+		void CreateStructType()
+		{
+			if (!_isSetBody) { return; }
+			
+			_isSetBody = true;
+			_type->setBody(_typeLayout);
 		}
 
-		virtual ~Struct() {}
+		llvm::StructType* getStructType() const { return _isSetBody ? _type : nullptr; }
+		
+		/// TODO : Protocol conform
+		void conformProtocol(/*Protocol* prot*/)
+		{
+		}
 	};
 }
 
